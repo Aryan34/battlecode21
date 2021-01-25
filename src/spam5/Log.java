eb1a4aada7a1e7d07572f7072828fbea4367e389
Aryan34 <44515360+Aryan34@users.noreply.github.com>
2021-01-25 02:27:14 -0500

    Add spam5 for testing new changes

---
+++ src/spam5/Log.java
@@ -0,0 +1,30 @@
+package spam5;
+
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
+
+public class Log {
+
+    static boolean toggle_on = false;
+    static RobotController rc = null;
+    static boolean debug = false;
+
+    public static void log(String str) {
+        if (toggle_on) {
+            System.out.println(str);
+        }
+//        RobotType[] showLogs = {RobotType.POLITICIAN, RobotType.ENLIGHTENMENT_CENTER};
+        RobotType[] showLogs = {};
+        for(RobotType type : showLogs){
+            if(rc.getType() == type){
+                System.out.println(str);
+            }
+        }
+    }
+
+    public static void debug(String str){
+        if(debug){
+            System.out.println(str);
+        }
+    }
+}
